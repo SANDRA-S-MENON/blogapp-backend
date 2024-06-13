5feddcee3c9fdb7b1594e629140535a7edc2dcd7
SANDRA-S-MENON <sandramenon2002@gmail.com>
2024-06-13 15:14:50 +0530

    signin completed

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ mongoose.connect("mongodb+srv://sandras02:sandrasmenon@cluster0.3g103sn.mongodb.
 const app=express()
 app.use(cors())
 app.use(express.json())
+const jwt=require("jsonwebtoken")
 
 
 const generateHashedpassword=async (password)=>{
     const salt=await bcryptjs.genSalt(10)
      return bcryptjs.hash(password,salt)
 }
+// api for signup
 
 app.post("/signup",async(req,res)=>{
     let input = req.body
@@ -23,6 +25,39 @@ app.post("/signup",async(req,res)=>{
     let blog= new blogmodel((input))
     blog.save()
     res.json({"status":"success"})
+})
+// api for sign in
+
+app.post("/signin",async(req,res)=>{
+   let input=req.body
+   blogmodel.find({"emailid":req.body.emailid}).then(
+    (response)=>{
+        if (response.length>0) {
+            let dbpassword = response[0].password
+            console.log(response)
+            bcryptjs.compare(input.password,dbpassword,(error,isMatch)=>{
+                if (isMatch) {
+                 jwt.sign({email:input.emailid},"blog-app",{expiresIn:"1d"},(error,token)=>{
+                    if (error) {
+                        res.json({"status":"unable to create token"})
+                        
+                    } else {
+                        res.json({"status":"success",userId:response[0]._id,"token":token})
+                    }
+                 }) 
+
+
+
+                } else {
+                    res.json({"status":"incorrect"})
+                }
+            })
+            
+        } else {
+            res.json({"status":"user does not exist"})
+        }
+    }
+   ).catch()
 })
 
 
